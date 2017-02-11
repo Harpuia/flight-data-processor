5792c379ae8a01c31629773376a14bd3d42a78e5
Yuchao Zhou <yuchaoz@andrew.cmu.edu>
2017-02-10 20:00:50 -0500

    Finished and tested the MonkeyByteManager class

--- src/MonkeysByteManager.java
+++ src/MonkeysByteManager.java
@@ -26,4 +26,34 @@ public class MonkeysByteManager {
         }
         return result;
     }
+
+    public static byte[] IntToBytes(int input) {
+        final int length = 4;
+        byte tm_stp[] = new byte[length];
+        for (int i = 0; i < length; i++) {
+            tm_stp[i] = (byte) (input >> 8 * (length - 1 - i));
+        }
+        return tm_stp;
+    }
+
+    public static byte[] LongToBytes(long input) {
+        final int length = 8;
+        byte tm_stp[] = new byte[length];
+        for(int i = 0; i < length; i++){
+            tm_stp[i] = (byte) (input >> 8 * (length - 1 - i));
+        }
+        return tm_stp;
+    }
+
+    public static void testInt(int input) {
+        if(input == BytesToInt(IntToBytes(input))) {
+            System.out.println("Test passed: int type");
+        }
+    }
+
+    public static void testLong(long input) {
+        if(input == BytesToLong(LongToBytes(input))) {
+            System.out.println("Test passed: long type");
+        }
+    }
 }
