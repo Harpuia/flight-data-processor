dbc4920479f4d8970f9fd95406ad5cd7d66f8f7d
Yazid Hamdi <yazid.hamdi@outlook.com>
2017-02-14 14:40:36 -0500

    Formatted MonkeysByteManager

--- src/MonkeysByteManager.java
+++ src/MonkeysByteManager.java
@@ -1,8 +1,13 @@
 /**
  * Created by yazid on 10-Feb-17.
+ * Groups operations needed for byte transformation in order to read the sensor data
  */
 public class MonkeysByteManager {
-
+    /**
+     * Reads bytes into an integer
+     * @param bytes Bytes representing the integer value
+     * @return Integer value of bytes
+     */
     public static int BytesToInt(byte[] bytes){
         int result = 0;
         for (int i = 0; i < bytes.length; i++) {
@@ -15,6 +20,11 @@ public class MonkeysByteManager {
         return result;
     }
 
+    /**
+     * Reads bytes into a long
+     * @param bytes Bytes representing the long value
+     * @return Long value of bytes
+     */
     public static long BytesToLong(byte[] bytes){
         long result = 0;
         for (int i = 0; i < bytes.length; i++) {
@@ -27,6 +37,11 @@ public class MonkeysByteManager {
         return result;
     }
 
+    /**
+     * Transforms an integer to a byte array
+     * @param input Integer to transform
+     * @return Output bytes
+     */
     public static byte[] IntToBytes(int input) {
         final int length = 4;
         byte tm_stp[] = new byte[length];
@@ -36,6 +51,11 @@ public class MonkeysByteManager {
         return tm_stp;
     }
 
+    /**
+     * Transforms an long to a byte array
+     * @param input Long to transform
+     * @return Output bytes
+     */
     public static byte[] LongToBytes(long input) {
         final int length = 8;
         byte tm_stp[] = new byte[length];
@@ -45,12 +65,20 @@ public class MonkeysByteManager {
         return tm_stp;
     }
 
+    /**
+     * Test method to verify if int transformations work
+     * @param input Integer input
+     */
     public static void testInt(int input) {
         if(input == BytesToInt(IntToBytes(input))) {
             System.out.println("Test passed: int type");
         }
     }
 
+    /**
+     * Test method to verify if long transformations work
+     * @param input Long input
+     */
     public static void testLong(long input) {
         if(input == BytesToLong(LongToBytes(input))) {
             System.out.println("Test passed: long type");
