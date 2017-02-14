9fc338fcd9cdea98f20d07c703046e2d16c327df
Yazid Hamdi <yazid.hamdi@outlook.com>
2017-02-14 00:34:02 -0500

    Fixed sink, cleaned up code

--- src/MonkeysSinkFilter.java
+++ src/MonkeysSinkFilter.java
@@ -38,26 +38,20 @@ public class MonkeysSinkFilter extends MonkeysFilterFramework {
     }
 
     public void run() {
-        /************************************************************************************
-         *	TimeStamp is used to compute time using java.util's Calendar class.
-         * 	TimeStampFormat is used to format the time value so that it can be easily printed
-         *	to the terminal.
-         *************************************************************************************/
-
         Calendar TimeStamp = Calendar.getInstance();
         SimpleDateFormat TimeStampFormat = new SimpleDateFormat("yyyy MM dd::hh:mm:ss:SSS");
 
         byte[] byteArray;
 
-        int MeasurementLength = 8;        // This is the length of all measurements (including time) in bytes
-        int IdLength = 4;                // This is the length of IDs in the byte stream
+        int MeasurementLength = 8;
+        int IdLength = 4;
 
-        byte databyte = 0;                // This is the data byte read from the stream
-        int bytesread = 0;                // This is the number of bytes read from the stream
+        byte databyte = 0;
+        int bytesread = 0;
 
-        long measurement;                // This is the word used to store all measurements - conversions are illustrated.
-        int id;                            // This is the measurement id
-        int i;                            // This is a loop counter
+        long measurement;
+        int id;
+        int i;
 
         File file=null;
         Writer writer=null;
@@ -75,108 +69,55 @@ public class MonkeysSinkFilter extends MonkeysFilterFramework {
         {
 
         }
-
-
-        /*************************************************************
-         *	First we announce to the world that we are alive...
-         **************************************************************/
-
         System.out.print("\n" + this.getName() + "::Sink Reading ");
 
         while (true) {
 
             try {
-                /***************************************************************************
-                 // We know that the first data coming to this filter is going to be an ID and
-                 // that it is IdLength long. So we first decommutate the ID bytes.
-                 ****************************************************************************/
-
                 id = 0;
 
                 for (i = 0; i < IdLength; i++) {
-                    databyte = ReadFilterInputPort();    // This is where we read the byte from the stream...
+                    databyte = ReadFilterInputPort();
 
-                    id = id | (databyte & 0xFF);        // We append the byte on to ID...
+                    id = id | (databyte & 0xFF);
 
-                    if (i != IdLength - 1)                // If this is not the last byte, then slide the
-                    {                                    // previously appended byte to the left by one byte
-                        id = id << 8;                    // to make room for the next byte we append to the ID
+                    if (i != IdLength - 1)
+                    {
+                        id = id << 8;
 
-                    } // if
+                    }
 
-                    bytesread++;                        // Increment the byte count
+                    bytesread++;
 
-                } // for
-
-                /****************************************************************************
-                 // Here we read measurements. All measurement data is read as a stream of bytes
-                 // and stored as a long value. This permits us to do bitwise manipulation that
-                 // is neccesary to convert the byte stream into data words. Note that bitwise
-                 // manipulation is not permitted on any kind of floating point types in Java.
-                 // If the id = 0 then this is a time value and is therefore a long value - no
-                 // problem. However, if the id is something other than 0, then the bits in the
-                 // long value is really of type double and we need to convert the value using
-                 // Double.longBitsToDouble(long val) to do the conversion which is illustrated.
-                 // below.
-                 *****************************************************************************/
-
+                }
                 measurement = 0;
 
                 for (i = 0; i < MeasurementLength; i++) {
                     databyte = ReadFilterInputPort();
-                    measurement = measurement | (databyte & 0xFF);    // We append the byte on to measurement...
+                    measurement = measurement | (databyte & 0xFF);
 
-                    if (i != MeasurementLength - 1)                    // If this is not the last byte, then slide the
-                    {                                                // previously appended byte to the left by one byte
-                        measurement = measurement << 8;                // to make room for the next byte we append to the
-                        // measurement
-                    } // if
+                    if (i != MeasurementLength - 1)
+                    {
+                        measurement = measurement << 8;
+                    }
 
-                    bytesread++;                                    // Increment the byte count
+                    bytesread++;
 
-                } // if
-
-                /****************************************************************************
-                 // Here we look for an ID of 0 which indicates this is a time measurement.
-                 // Every frame begins with an ID of 0, followed by a time stamp which correlates
-                 // to the time that each proceeding measurement was recorded. Time is stored
-                 // in milliseconds since Epoch. This allows us to use Java's calendar class to
-                 // retrieve time and also use text format classes to format the output into
-                 // a form humans can read. So this provides great flexibility in terms of
-                 // dealing with time arithmetically or for string display purposes. This is
-                 // illustrated below.
-                 ****************************************************************************/
-
+                }
                 if (id == 0) {
                     TimeStamp.setTimeInMillis(measurement);
 
 
-                } // if
+                }
 
-                /****************************************************************************
-                 // Here we pick up a measurement (ID = 3 in this case), but you can pick up
-                 // any measurement you want to. All measurements in the stream are
-                 // decommutated by this class. Note that all data measurements are double types
-                 // This illustrates how to convert the bits read from the stream into a double
-                 // type. Its pretty simple using Double.longBitsToDouble(long value). So here
-                 // we print the time stamp and the data associated with the ID we are interested
-                 // in.
-                 ****************************************************************************/
-                //TODO: save file instead of displaying
-                //System.out.println(TimeStampFormat.format(TimeStamp.getTime()) + " ID = " + id + " " + Double.longBitsToDouble(measurement));
-                else if(id==3 && displayWildPoints) {
+                else if(id==3 && displayWildPoints && measurement < 0) {
                     writer.write(TimeStampFormat.format(TimeStamp.getTime()) + "  " +String.valueOf(new DecimalFormat("#0.00000").format(-Double.longBitsToDouble(measurement))) + "* \n");
-//                    writer.write(String.valueOf(Double.longBitsToDouble(measurement)) + "\n");
                     writer.flush();
                 }
 
                 else {
                     writer.write(TimeStampFormat.format(TimeStamp.getTime()) + "  " +String.valueOf(new DecimalFormat("#0.00000").format(Double.longBitsToDouble(measurement))) + "\n");
-//                    writer.write(String.valueOf(Double.longBitsToDouble(measurement)) + "\n");
                     writer.flush();
-
-
-                    // try
                 }
 
             }
@@ -184,8 +125,6 @@ public class MonkeysSinkFilter extends MonkeysFilterFramework {
             {
 
             }
-
-
             catch(EndOfStreamException e)
             {
                 try {
@@ -196,18 +135,8 @@ public class MonkeysSinkFilter extends MonkeysFilterFramework {
                 } catch (IOException ioe) {
 
                 }
+            }
+        }
+    }
 
-
-            } // catch
-
-
-
-
-
-
-        } // while
-
-
-    } // run
-
-} // SingFilter
+}
