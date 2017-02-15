34bd2c6c44e5684d95d5b4676cfe7a1d58d1b498
Yuchao Zhou <yuchaoz@andrew.cmu.edu>
2017-02-14 23:54:08 -0500

    Merger and frame comparator commented

--- src/MonkeysFrameComparator.java
+++ src/MonkeysFrameComparator.java
@@ -1,9 +1,17 @@
 import java.util.Comparator;
 
 /**
- * Created by Yuchao on 12/02/2017.
+ * This class is the comparator to define the compare operation of class MonkeysFrame.
+ * We compare the MonkeysFrame by comaring the time.
  */
 public class MonkeysFrameComparator implements Comparator<MonkeysFrame> {
+
+    /**
+     *
+     * @param f1 the MonkeysFrame object passed in
+     * @param f2 the MonkeysFrame object passed in
+     * @return -1 if f1 smaller than f2, 0 if f1 equals to f2, and 1 if f1 larger than f2.
+     */
 
     @Override
     public int compare (MonkeysFrame f1, MonkeysFrame f2) {
@@ -11,6 +19,7 @@ public class MonkeysFrameComparator implements Comparator<MonkeysFrame> {
             return 0;
         }
 
+        /** If the time of f1 is smaller than that in f2, we say f1 is smaller than f2. **/
         return f1.time < f2.time ? -1 : 1;
     }
 }
--- src/MonkeysMerger.java
+++ src/MonkeysMerger.java
@@ -2,125 +2,122 @@ import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
 
 /**
- * Created by Yuchao on 10/02/2017.
+ * MonkeysMerger is a filter that takes input from two filters and merge the results sorted increasingly based on time.
  */
-public class MonkeysMerger extends MonkeysFilterFramework
-{
+public class MonkeysMerger extends MonkeysFilterFramework {
+    /**
+     * Input ports, input filter instances and output port.
+     * MeasurementLength is the length of all measurements (including time) in bytes
+     * IdLength is the length of IDs in the byte stream
+     * frameCol refers to how many measurements we have in one frame.
+     **/
     private PipedInputStream InputReadPort1 = new PipedInputStream();
     private PipedInputStream InputReadPort2 = new PipedInputStream();
     private PipedOutputStream OutputWritePort = new PipedOutputStream();
-    private final int MeasurementLength = 8;        // This is the length of all measurements (including time) in bytes
-    private final int IdLength = 4;                // This is the length of IDs in the byte stream
+    private final int MeasurementLength = 8;
+    private final int IdLength = 4;
     private final int frameCol = 6;
     private MonkeysFilterFramework InputFilter1;
     private MonkeysFilterFramework InputFilter2;
 
-
-    private boolean EndOfInputStream(MonkeysFilterFramework InputFilter)
-    {
-        if (InputFilter.isAlive())
-        {
+    /**
+     * This function identifies if the stream reaches to the end of the input.
+     * @param InputFilter specifies which input filter we read from.
+     * @return true if the filter reaches the end of the input.
+     */
+    private boolean EndOfInputStream(MonkeysFilterFramework InputFilter) {
+        if (InputFilter.isAlive()) {
             return false;
 
         } else {
 
             return true;
 
-        } // if
+        }
 
-    } // EndOfInputStream
+    }
 
-    void Connect (MonkeysFilterFramework Filter1, MonkeysFilterFramework Filter2)
-    {
-        try
-        {
-            // Connect this filter's input to the upstream pipe's output stream
-
+    /**
+     * This is the overloaded function from MonkeysFilterFramework
+     * @param Filter1 is the first filter it reads from
+     * @param Filter2 is the second filter it reads from
+     */
+    void Connect(MonkeysFilterFramework Filter1, MonkeysFilterFramework Filter2) {
+        /** The merger will connect to the port of both filter1 and filter2. **/
+        try {
             InputReadPort1.connect(Filter1.OutputWritePort);
             InputReadPort2.connect(Filter2.OutputWritePort);
             InputFilter1 = Filter1;
             InputFilter2 = Filter2;
-        } // try
+        }
 
-        catch( Exception Error )
-        {
-            System.out.println( "\n" + this.getName() + " FilterFramework error connecting::"+ Error );
+        catch (Exception Error) {
+            System.out.println("\n" + this.getName() + " FilterFramework error connecting::" + Error);
 
-        } // catch
+        }
 
-    } // Connect
+    }
 
-    byte ReadFilterInputPort(MonkeysFilterFramework InputFilterFramework, PipedInputStream InputReadPort, int id) throws EndOfStreamException
-    {
+    /**
+     * This is the overloaded function from MonkeysFilterFramework
+     * @param InputFilterFramework specifies the input filter it reads from.
+     * @param InputReadPort specifies the port it reads from.
+     * @param id is 1 if we read from filter1, 2 if we read from filter2.
+     * @return a byte of information we read
+     * @throws EndOfStreamException
+     */
+    byte ReadFilterInputPort(MonkeysFilterFramework InputFilterFramework, PipedInputStream InputReadPort, int id) throws EndOfStreamException {
         byte datum = 0;
 
-        /***********************************************************************
-         * Since delays are possible on upstream filters, we first wait until
-         * there is data available on the input port. We check,... if no data is
-         * available on the input port we wait for a quarter of a second and check
-         * again. Note there is no timeout enforced here at all and if upstream
-         * filters are deadlocked, then this can result in infinite waits in this
-         * loop. It is necessary to check to see if we are at the end of stream
-         * in the wait loop because it is possible that the upstream filter completes
-         * while we are waiting. If this happens and we do not check for the end of
-         * stream, then we could wait forever on an upstream pipe that is long gone.
-         * Unfortunately Java pipes do not throw exceptions when the input pipe is
-         * broken.
-         ***********************************************************************/
-
-        try
-        {
-            while (InputReadPort.available()==0 )
-            {
-                if (EndOfInputStream(InputFilterFramework))
-                {
+        try {
+            while (InputReadPort.available() == 0) {
+                if (EndOfInputStream(InputFilterFramework)) {
                     throw new EndOfStreamException("Port " + id + ": End of input stream reached");
 
-                } //if
+                }
 
-                //sleep(250);
+                sleep(250);
 
-            } // while
+            }
 
-        } // try
+        }
 
-        catch( EndOfStreamException Error )
-        {
+        catch (EndOfStreamException Error) {
             throw Error;
 
-        } // catch
+        }
 
-        catch( Exception Error )
-        {
-            System.out.println( "\n" + this.getName() + " Error in read port " + id + " wait loop::" + Error );
+        catch (Exception Error) {
+            System.out.println("\n" + this.getName() + " Error in read port " + id + " wait loop::" + Error);
 
-        } // catch
+        }
 
-        /***********************************************************************
-         * If at least one byte of data is available on the input
-         * pipe we can read it. We read and write one byte to and from ports.
-         ***********************************************************************/
-
-        try
-        {
-            datum = (byte)InputReadPort.read();
+        try {
+            datum = (byte) InputReadPort.read();
             return datum;
 
-        } // try
+        }
 
-        catch( Exception Error )
-        {
-            System.out.println( "\n" + this.getName() + " Pipe read error::" + Error );
+        catch (Exception Error) {
+            System.out.println("\n" + this.getName() + " Pipe read error::" + Error);
             return datum;
 
-        } // catch
+        }
 
-    } // ReadFilterPort
+    }
 
 
-    private MonkeysFrame readOneFrame (MonkeysFilterFramework InputFilter, PipedInputStream port, int id) throws EndOfStreamException {
+    /**
+     * This function reads one frame of data from a specified input filter, and returns as a MonkeysFrame format
+     * @param InputFilter
+     * @param port
+     * @param id
+     * @return
+     * @throws EndOfStreamException
+     */
+    private MonkeysFrame readOneFrame(MonkeysFilterFramework InputFilter, PipedInputStream port, int id) throws EndOfStreamException {
         long[] frameData = new long[frameCol];
-        for(int i = 0; i < frameCol; i++) {
+        for (int i = 0; i < frameCol; i++) {
             byte[] byteBuffer = new byte[MeasurementLength];
 
             for (int j = 0; j < IdLength; j++) {
@@ -135,7 +132,11 @@ public class MonkeysMerger extends MonkeysFilterFramework
         return new MonkeysFrame(frameData);
     }
 
-    private void writeOneFrame (MonkeysFrame frame) {
+    /**
+     * This function writes one frame each time.
+     * @param frame is the MonkeysFrame object we want to write.
+     */
+    private void writeOneFrame(MonkeysFrame frame) {
         long[] frameData = frame.toLongArray();
         for (int i = 0; i < frameCol; i++) {
             byte[] byteBuffer = MonkeysByteManager.IntToBytes(i);
@@ -144,59 +145,61 @@ public class MonkeysMerger extends MonkeysFilterFramework
             }
 
             byteBuffer = MonkeysByteManager.LongToBytes(frameData[i]);
-            for(int j = 0; j < byteBuffer.length; j++) {
+            for (int j = 0; j < byteBuffer.length; j++) {
                 WriteFilterOutputPort(byteBuffer[j]);
             }
         }
     }
 
-    void ClosePorts(PipedInputStream InputReadPort)
-    {
-        try
-        {
+    /**
+     * This is the overloaded the function from MonkeysFilterFramework.
+     * @param InputReadPort specifies which input port we want to close.
+     */
+    void ClosePorts(PipedInputStream InputReadPort) {
+        try {
             InputReadPort.close();
             OutputWritePort.close();
 
+        } catch (Exception Error) {
+            System.out.println("\n" + this.getName() + " ClosePorts error::" + Error);
+
         }
-        catch( Exception Error )
-        {
-            System.out.println( "\n" + this.getName() + " ClosePorts error::" + Error );
 
-        } // catch
+    }
 
-    } // ClosePorts
+    public void run() {
+        /** boolean variables we need to use. **/
+        boolean hasNext1 = true, hasNext2 = true, firstTime = true;
 
-    public void run()
-    {
-        //Declarations
-        boolean hasNext1 = true, hasNext2 = true;
-        boolean firstTime = true;
+        /** frameA is the frame read from filter1. frameB is the frame read from filter2. **/
         MonkeysFrame frameA = null;
         MonkeysFrame frameB = null;
-        System.out.print( "\n" + this.getName() + "::Merging ");
-        while (hasNext1 && hasNext2)
-        {
-            try
-            {
-                if(firstTime) {
+        System.out.print("\n" + this.getName() + "::Merging ");
+
+        /**
+         * Here we do the merging by buffering one frame from each filter.
+         * Then we compare these two frames, and always write the smaller frame.
+         * */
+        while (hasNext1 && hasNext2) {
+            try {
+                /** In the first time, we read frames from the ports. **/
+                if (firstTime) {
                     frameA = readOneFrame(InputFilter1, InputReadPort1, 1);
                     frameB = readOneFrame(InputFilter2, InputReadPort2, 2);
                     firstTime = false;
                 }
-                if(frameA.time <= frameB.time) {
+
+                if (frameA.time <= frameB.time) {
                     writeOneFrame(frameA);
                     frameA = readOneFrame(InputFilter1, InputReadPort1, 1);
                 } else {
                     writeOneFrame(frameB);
                     frameB = readOneFrame(InputFilter2, InputReadPort2, 2);
                 }
-            }
-
-            catch (EndOfStreamException e)
-            {
-                if(e.getMessage().startsWith("Port 1")) {
+            } catch (EndOfStreamException e) {
+                if (e.getMessage().startsWith("Port 1")) {
                     hasNext1 = false;
-                } else if(e.getMessage().startsWith("Port 2")) {
+                } else if (e.getMessage().startsWith("Port 2")) {
                     hasNext2 = false;
                 } else {
                     e.printStackTrace();
@@ -204,9 +207,10 @@ public class MonkeysMerger extends MonkeysFilterFramework
             }
         } // while
 
+        /** If the first port still has remaining data, we write them all. **/
         if (hasNext1) {
             writeOneFrame(frameA);
-            while(true) {
+            while (true) {
                 try {
                     byte databyte = ReadFilterInputPort(InputFilter1, InputReadPort1, 1);
                     WriteFilterOutputPort(databyte);
@@ -215,11 +219,13 @@ public class MonkeysMerger extends MonkeysFilterFramework
                     break;
                 }
             }
-        } else if (hasNext2) {
-            writeOneFrame(frameB);
-            while(true) {
-                try {
+        }
 
+        /** Otherwise we write data from the second port. **/
+        else if (hasNext2) {
+            writeOneFrame(frameB);
+            while (true) {
+                try {
                     byte databyte = ReadFilterInputPort(InputFilter2, InputReadPort2, 2);
                     WriteFilterOutputPort(databyte);
                 } catch (EndOfStreamException e) {
